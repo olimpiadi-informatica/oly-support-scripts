d15e90e2680a12e17f3d921edfc37813194ed3b2
Luca Versari <veluca93@gmail.com>
2025-07-06 13:37:04 +0200

    More parallelism in backups

--- backups/update.sh
+++ backups/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e                                                         
 
-parallel -j8 ../do_single_update.sh < ~/contestants.hosts || true
+parallel -j32 ../do_single_update.sh < ~/contestants.hosts || true
 git add -A
 git commit -m "$(date)" --allow-empty
